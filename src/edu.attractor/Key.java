fa8e4b4bf5ccadd58febffef57e41b110c0b64ee
kmeruet <k_meruet@mail.ru>
2021-09-18 01:42:27 +0600

    test5 commit

--- src/edu.attractor/Key.java
+++ src/edu.attractor/Key.java
@@ -1,4 +1,19 @@
 package edu.attractor;
 
 public class Key {
+    String key;
+    String value;
+
+    public Key(String key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public String getValue() {
+        return value;
+    }
 }
