68fe783a7210112512cd113ba1347ca995e63136
kmeruet <k_meruet@mail.ru>
2021-09-18 01:42:00 +0600

    test4 commit

--- src/Connactionable.java
+++ src/Connactionable.java
@@ -1,2 +1,13 @@
 public interface Connactionable {
+    void open();
+    void close();
+    void checkDB();
+    void read();
+    void checkOneRecording();
+    void readRecordOfKey();
+    void readFromBase(int first, int finaly);
+    void countRecord();
+    void addRecord();
+    void updateIntRecordOfId();
+    void updateStringRecordOfKey();
 }
